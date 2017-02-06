dff481294bd77741f942b9c876e72f5a61da8367
likewind1993 <byroncheng@foxmail.com>
2017-02-06 21:43:48 +0800

    2017-2-6, Fix some Bugs

--- 1003.cpp
+++ 1003.cpp
@@ -1,219 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAXINT 65535
-#define MAX 100
+#define INF 0x7fffffff
+#define MAX 10000
 
-typedef struct List
-{
-	int num;
-	int weight;
-	struct List * next;
-}List;
-typedef struct Node
-{
-	int num;
-	int teams;
-	struct List * next;	
-	
-}Node;
+int max_amount;
+int city_nums, road_nums;
+int start, end;
+int graph[MAX][MAX]={0};
+int terms[MAX];
+int visit[MAX];
+int count;
+int min_dist = INF;
 
-
-void graphCreate(Node * list, int sites, int routes)
+void dfs(int start, int dist, int amount)
 {
-	int from, to, weight;
-	List * p;
-	for(int i = 0; i<routes; i++)
+	if(start == end)
 	{
-		scanf("%d %d %d", &from, &to, &weight);
-		if(list[from].next == NULL)
+		if(dist < min_dist)
 		{
-			List * temp = (List *)malloc(sizeof(List));
-			temp->num = to;
-			temp->weight = weight;
-			temp->next = NULL;
-			list[from].next = temp;
-		}
-		else
-		{
-			p = list[from].next;
-			while(p->next != NULL)
-				p = p->next;
-			List * temp = (List *)malloc(sizeof(List));
-			temp->num = to;
-			temp->weight = weight;
-			temp->next = NULL;
-			p->next = temp;
-		}
-		
-		if(list[to].next == NULL)
-		{
-			List * temp = (List *)malloc(sizeof(List));
-			temp->num = from;
-			temp->weight = weight;
-			temp->next = NULL;
-			list[to].next = temp;
-		}
-		else
-		{
-			p = list[to].next;
-			while(p->next != NULL)
-				p = p->next;
-			List * temp = (List *)malloc(sizeof(List));
-			temp->num = from;
-			temp->weight = weight;
-			temp->next = NULL;
-			p->next = temp;
-		}
-		
-	}
-}
-
-int length(Node * list, int from, int to)
-{
-	List * p = (List *)malloc(sizeof(List));
-	if(list[from].next == NULL)
-		return MAXINT;
-	p = list[from].next;
-	while(p != NULL)
-	{
-		if(p->num == to)
-			return p->weight;
-		p = p->next; 
-	}
-	return MAXINT;
-}
-
-void remove(int * p, int position)
-{
-	p[position] = -1;
-}
-void add(int * p, int position)
-{
-	p[position] = -1;
-}
-int shortest(Node * list, int sites, int routes, int from, int to)
-{
-	//S is Remain, U is Begin
-	int S[MAX];
-	for(int i = 0; i<sites; i++)
-		S[i] = 1;
-	int U[MAX]; 
-	memset(U, -1, sizeof(U));
-	int dist[MAX];
-	for(int i = 0; i<MAX; i++)
-		dist[i] = MAXINT;
-	S[from] = -1;
-	U[from] = 1;
-	
-	
-//	printf("\n");
-//	for(int i = 0; i<sites; i++)
-//	{
-//		printf("%d ", U[i]);
-//	}
-	
-	int temp_site = -1;
-	int shortest = MAX;
-	List * p = (List *)malloc(sizeof(List));
-	p = list[from].next;
-	
-//	for(int i = 0; i<sites; i++)
-//	{
-//		if(i == from)
-//			U[i] = 1;
-//		else
-//			S[i] = 1;
-//	}
-//	printf("\n");
-//	for(int i = 0; i<sites; i++)
-//	{
-//		printf("%d ", U[i]);
-//	}
-//	printf("\n");
-//	for(int i = 0; i<sites; i++)
-//	{
-//		printf("%d ", S[i]);
-//	}
-//	printf("\n");
-//	while(p != NULL)
-//	{	
-//		printf("\nfrom %d to %d weight %d\n", from, p->num, p->weight);
-//		p = p->next;
-//	}
-	for(int i = 0; i<sites; i++)
-	{
-		if(i == from)
-			continue;
-		int length_temp = length(list, from, i);
-		if(length_temp != MAXINT)
-		{
-//			printf("\n%d %d\n", i, length_temp);
-
-			dist[i] = length_temp;
-			if(shortest > length_temp)
-			{
-				shortest = length_temp;
-				temp_site = i;
-			}
+			count = 1;
+			min_dist = dist;
+			max_amount = amount;
 			
-		}
-	}
-	printf("dist is \n");
-	for(int i = 0; i<sites; i++)
-		printf("%d ", dist[i]);
-	
-	printf("\n%d ", temp_site);
-	
-	U[temp_site] = 1;
-	S[temp_site] = -1;
-	
-	int short_temp = shortest;
-	
-	for(int i = 0; i<sites; i++)
-	{
-		if(S[i] == -1)
-			continue;
-		int length_temp = length(list, temp_site, i);
-		if(length_temp != MAXINT)
+		}else if(dist == min_dist)
 		{
-//			printf("\n%d %d\n", i, length_temp);
-			short_temp = shortest + length_temp;
-			if(short_temp < dist[i])
-			{
-				dist[i] = short_temp;
-				temp_site = i;
-			}
+			count++;
+			if(amount > max_amount)
+				max_amount = amount;
 		}
+		return;	
 	}
-	
-	printf("dist is \n");
-	for(int i = 0; i<sites; i++)
-		printf("%d ", dist[i]);
-	
-	printf("\n%d ", temp_site);
-
-	
-	return 0;
-	
+	for(int i = 0; i<city_nums; i++)
+	{
+		if(graph[start][i] != 0 && !visit[i])
+		{
+			visit[i] = 1;
+			dfs(i, dist + graph[start][i], amount + terms[i]);
+			visit[i] = 0;
+		}
+	}	
 }
+
 
 int main()
 {
-	Node list[MAX];
+
+	scanf("%d %d %d %d", &city_nums, &road_nums, &start, &end);
+	for(int i = 0; i<city_nums; i++)
+		scanf("%d", &terms[i]);
 	
-	int sites, routes;
-	int from, to;
-	scanf("%d %d %d %d", &sites, &routes, &from, &to);
-	for(int i = 0; i<sites; i++)
+	for(int i = 0; i<MAX; i++)
+		visit[i] = 0;
+	
+	for(int i = 0; i<road_nums; i++)
 	{
-		int team = 0;
-		scanf("%d", &team);
-		list[i].teams = team;
-		list[i].next = NULL; 
+		int start_temp, end_temp, weight;
+		scanf("%d %d %d", &start_temp, &end_temp, &weight);
+		graph[start_temp][end_temp] = graph[end_temp][start_temp] = weight;
 	}
-	graphCreate(list, sites, routes);
-	shortest(list, sites, routes, from, to);
+	
+	visit[start] = 1;
+	dfs(start, 0, terms[start]);
+	
+	printf("%d %d", count, max_amount);
 	
 	return 0;
 }
--- 1004.cpp
+++ 1004.cpp
@@ -1,39 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 100
-typedef struct FamiNode
-{
-	int level;
-	char ID[2];
-	FamiNode * next;
-}FamiNode;
+#include <cstdio>
+#include <map>
+#include <vector>
+#define MAX 1000
+using namespace std;
 
-typedef FamiNode * FamiTree;
+int total_nums, non_leaf;
+map<int, vector<int> >Map;
+int record[MAX] = {0};
 
-FamiTree listCreate(int total)
+
+void dfs(int id, int level)
 {
-	FamiTree line [MAX];
-	FamiTree * head = line;
-	
-	for(int i = 0; i<total; i++)
+	if(Map[id].empty())
 	{
-		FamiTree temp = (FamiTree)malloc(sizeof(FamiNode));
-		scanf("%s", &temp->ID);
-		temp->level = i;
-		int nums;
-		scanf("%d", &nums)
-		for(int j = 0; j<nums; j++)
-		{
-			
-		}
+		++record[level];
+		return;
 	}
-	
+	vector<int>::iterator iter;
+	for(iter = Map[id].begin(); iter<Map[id].end(); iter++)
+		dfs(*iter, level+1);	
 }
 
 int main()
 {
-	int total, non_leafnode;
-	scanf("%d %d", &total, &non_leafnode);
-	FamiTree tree = listCreate(non_leafnode);
+	scanf("%d %d", &total_nums, &non_leaf);
+	int leaf_nodes = total_nums - non_leaf;
+	int id, k;
+	for(int i = 0; i<non_leaf; i++)
+	{
+		scanf("%d %d", &id, &k);
+		int sub_id;
+		while(k--)
+		{
+			scanf("%d", &sub_id);	
+			Map[id].push_back(sub_id);
+		}
+	}
 	
+	dfs(1, 0);
+	int count = record[0];
+	printf("%d", record[0]);
+	for(int i = 1; count<leaf_nodes; i++)
+	{
+		printf(" %d", record[i]);
+		count += record[i];
+	}
+	return 0;
 }
